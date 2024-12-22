adaafa5fab9d3e0acb405749a43eb126aabc4a63
Dilisha-LN <dilishagimshan20@gmail.com>
2024-12-22 17:27:28 +0530

    😳  QUEEN-AHINSA-MD  😳

---
+++ plugins/about.js
@@ -0,0 +1,37 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+cmd({
+    pattern: "about",
+    react: "👑",
+    desc: "get owner dec",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let about = `*╭┈───────────────•*
+
+*👋 HELLO ${pushname}*
+
+*╰┈───────────────•*
+*╭┈───────────────•*
+*│  ◦* *WELCOME TO QUEEN-AHINSA-MD*
+*│  ◦* DONT ARE CRY*
+*│  ◦* *STATUES IN MINE PERSONEL*
+*│  ◦* *TITANIC POWER YAKUZA TEAM*
+*│  ◦* *STUDY IN JAVASCRIPT*
+*│  ◦* *MOD AS WHATSAPP*
+*│  ◦* *ᴀ sɪᴍᴘʟᴇ ᴡʜᴀᴛsᴀᴘᴘ ᴅᴇᴠᴇʟᴘᴏʀ*
+*╰┈───────────────•*
+> *◆◆◆◆◆◆◆◆◆◆◆◆*
+
+*──•⟢*
+`
+
+await conn.sendMessage(from,{image:{url:config.ALIVE_IMG},caption:about},{quoted:mek})
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
